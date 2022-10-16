5cba3ecd8731a322bead5809086065ab35c1c388
SwarupParua <108712030+SwarupParua@users.noreply.github.com>
2022-10-16 17:44:53 +0530

    Add files via upload

---
+++ VariableQs2.cpp
@@ -0,0 +1,16 @@
+// The user is asked to enter two integers and their sum is displayed
+
+#include <iostream>
+using namespace std;
+int main() {
+ int n1, n2, sum;
+ cout << "Enter n1 value: ";
+ cin >> n1;
+cout << "Enter n2 value: ";
+ cin >> n2;
+ // sum of two numbers is stored in variable sum
+ sum = n1 + n2;
+// prints sum 
+ cout <<"The sum of two number is :" << sum <<endl; 
+ return 0;
+}
