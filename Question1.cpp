52d212843f0e694ec4a90ec2f5c498ac0b555c05
SwarupParua <108712030+SwarupParua@users.noreply.github.com>
2022-10-16 17:50:34 +0530

    Add files via upload

---
+++ Question1.cpp
@@ -0,0 +1,11 @@
+// Take 2 integer values in two variables x and y and print their product .
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=4,y=5;
+    cout<<"The product value of x and y is :"<<x*y<<endl;
+    return 0;
+}
